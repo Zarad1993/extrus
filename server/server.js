56f881efb3a5ba1b351f83334af9bebbb0dbea41
Mohammad Albakri <mohammad.albakri93@gmail.com>
2016-07-17 19:50:14 +0300

    Resolve conflicts

--- server/server.js
+++ server/server.js
@@ -8,11 +8,13 @@ var app = express();
 mongoose.connect(mongoURI);
 
 // configure our server with all the middleware and routing
-require('./config/middleware.js')(app, express);
-require('./config/routes.js')(app, express);
+// require('./config/middleware.js')(app, express);
+// require('./config/routes.js')(app, express);
 
 // start listening to requests on port 8000
 app.listen(8000);
-
+mongoose.on('open', function(){
+	console.log('mongoose now connected');
+})
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
